0f351141f567d2a84a8729a0b71cbb9d8d20d650
Sibuchanda <sibuchanda457@gmail.com>
2025-02-27 17:51:29 +0530

    Day-13[Advanced option in useReducer

--- src/useReducer/index.jsx
+++ src/useReducer/index.jsx
@@ -3,24 +3,54 @@ import { useReducer } from "react";
 
 const ExampleUseReducer = ()=>{
 
+const initialstate = {
+    count : 0,
+    incre: 3,
+    decre: 3,
+}
+
 const reducer = (state, action)=>{
-    if(action.type === 'INCREMENT'){
-        return state + 1;
-    }
-    if(action.type === 'DECREMENT'){
-        return state - 1;
+    // if(action.type === 'INCREMENT'){
+    //     return state + 1;
+    // }
+    // if(action.type === 'DECREMENT'){
+    //     return state - 1;
+    // }
+    // if(action.type === 'RESET'){
+    //     return state = 0;
+    // }
+
+    switch(action.type){
+        case "INCREMENT":
+            return {
+                ...state,
+                count : state.count + 1
+            }
+        case "DECREMENT":
+            return {
+                ...state,
+                count : state.count - 1
+            }
+        case "RESET":
+            return {
+                ...state,
+                count : 0
+            }
+        default:
+            return state;
     }
 }
 
-const [value, dispatch] = useReducer(reducer, 0);
+const [state, dispatch] = useReducer(reducer, initialstate);
 
     return(
         <>
          <div className="container bg-amber-100 flex flex-col justify-center items-center h-screen w-screen">
             <h1 className="text-4xl">Example of useReducer Hook in React 19</h1>
-            <h2 className="mt-2 text-2xl">Value : {value} </h2>
+            <h2 className="mt-2 text-2xl">Value : {state.count} </h2>
             <button onClick={()=> dispatch({type: "INCREMENT"})} className="mt-4 w-auto h-auto p-2 border cursor-pointer hover:bg-gray-200">Increment</button>
             <button onClick={()=> dispatch({type: "DECREMENT"})} className="mt-4 w-auto h-auto p-2 border cursor-pointer hover:bg-gray-200">Decrement</button>
+            <button onClick={()=> dispatch({type: "RESET"})} className="mt-4 w-auto h-auto p-2 border cursor-pointer hover:bg-gray-200">RESET</button>
          </div>
         </>
     )
