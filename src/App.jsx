fd38222730cc06f08b02bf822ab029d01808eb21
Sibuchanda <sibuchanda457@gmail.com>
2025-02-28 12:25:07 +0530

    Day-14[React Router part - 1]

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,29 @@
 import { useState } from 'react'
-import Increment from './useMemo/Increment'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import Contactus from './ReactRouter/Contactus.jsx';
+import About from './ReactRouter/About.jsx';
+import Home from './ReactRouter/Home.jsx';
 
 
 function App() {
 
-  return (
-    <>
-     <Increment/>
-    </>
-  )
+  const router = createBrowserRouter([
+    {
+      path: '/',
+      element: <Home/>,
+    },
+    {
+      path: '/about',
+      element: <About/>,
+    },
+    {
+      path: '/contact',
+      element: <Contactus/>,
+    }
+  ]);
+
+  return <RouterProvider router={router} />
 }
+
 
 export default App
---
+++ src/ReactRouter/Contactus.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Contactus = () => {
+  return (
+    <>
+    <h1>This is Contactus page</h1>
+    </>
+  )
+}
+
+export default Contactus;
