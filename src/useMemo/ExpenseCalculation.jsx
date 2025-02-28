706b1eeb22be4f3160156a5b1e7eff55a97efc04
Sibuchanda <sibuchanda457@gmail.com>
2025-02-28 08:31:57 +0530

    Day-14[program without useMemo]

---
+++ src/useMemo/ExpenseCalculation.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+
+const ExpenseCalculation = () => {
+
+    const sum = () => {
+        console.log("Calculating sum .....");
+        let i = 0;
+        for (i = 0; i <= 1000000000; i++) i = i + 1;
+
+        return i;
+    }
+
+    const total = sum();
+
+    return <h1>Total expense : {total} </h1>
+
+}
+
+export default ExpenseCalculation;
--- src/useMemo/Increment.jsx
+++ src/useMemo/Increment.jsx
@@ -1,23 +1,24 @@
 import React, { useState } from 'react'
-import DummyComponent from './DummyComponent';
+import ExpenseCalculation from './ExpenseCalculation';
+
+
+
 
 const Increment = () => {
 
-    const [value, setValue] = useState(0);
-
+    const [count, setCount] = useState(0);
 
   return (
     <>
-    <div className="container w-screen h-90 flex flex-col justify-evenly items-center bg-amber-100">
-        <h1 className='text-4xl font-bold'>Example of React memo</h1>
-        <div className="box">
-            <h1 className='text-2xl font-bold'>Value : {value}</h1>
-            <button onClick={()=>setValue((pre)=> pre + 1)} className='border w-auto h-auto p-2 cursor-pointer mt-4'>Increment</button>
-        </div>
+    <div className="container w-screen h-screen flex flex-col justify-center items-center bg-amber-100 gap-4">
+    <ExpenseCalculation />
+     <button onClick={()=> setCount((prev) => prev + 1)} className='w-auto h-auto p-2 bg-blue-500 hover:bg-blue-800 cursor-pointer'>Increment value </button>
+     <h2 className='text-3xl'>Value : {count}</h2>
+
     </div>
-   <DummyComponent/>
     </>
   )
 }
+
 
 export default Increment;
